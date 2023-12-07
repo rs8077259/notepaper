69c16769a21a7a8c1cfa59a192fc8b46814e997e
Raj Singh <rs8077259@gmail.com>
2023-12-08 03:03:51 +0530

    added menubar in page showing notes in browser

---
+++ paperapi/static/js/paper.js
@@ -0,0 +1,31 @@
+var startX=0
+var chapterListOpen=false
+var headingListOpen=false;
+let viewportWidth=window.innerWidth
+var container = document.querySelector('.content-area');
+var chapterlist = document.querySelector('.chapter-list');
+var headingList = document.querySelector('.heading-list');
+console.log(window)
+container.addEventListener('touchstart', handleTouchStart);
+container.addEventListener('touchend', handleTouchEnd);
+
+function handleTouchStart(event) {
+    startX = event.touches[0].clientX;
+    currentX = startX;
+}
+function handleTouchEnd(event){
+    console.log(viewportWidth)
+    console.log(startX);
+    let move=event.changedTouches[0].clientX-startX
+    console.log(move)
+    if(startX>=viewportWidth-10 || startX<=0+10)
+    if ((move>=-10&&move!=0) && chapterListOpen==false){
+        chapterlist.style.left = '0px';
+        chapterListOpen=true
+    }
+    else if((move<=10&&move!=0) && headingListOpen==false){
+        console.log(headingList)
+        headingList.style.right = '0px';
+        headingListOpen=true;
+    }
+}
